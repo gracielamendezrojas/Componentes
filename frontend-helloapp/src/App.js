8b7e244edd3c86a7187b4efead19691bb9a91264
IGNACIO JOSUE ZELADA ARAYA <izeladaa@ucenfotec.ac.cr>
2023-06-08 19:53:08 -0600

    UI LogIn

--- frontend-helloapp/src/App.js
+++ frontend-helloapp/src/App.js
@@ -33,29 +33,37 @@ function App() {
         </div>
         
       </header>
-      <div className='flexContainer flexSpaceAround'>
-        <div className = 'flexColumn flexSpaceAround color-blue padding'>
-          
-          <TextField  id="standard-basic" label="Email" variant="standard" />
+      <div className='flexContainer flexSpaceBetween'>
+        <div className = 'flexColumn flexSpaceEvenly b-color-purple dimensions '>
+
+          <Typography variant="h5" className='color-white'>
+            Log in
+          </Typography>
+
+          <TextField  id="standard-basic" label="Email"  variant="standard" />
           <TextField  id="standard-basic" label="Password" variant="standard" />
 
-          <Button  variant="contained" >          
+          <Button  variant="contained">
             <Typography variant="h5" gutterBottom>
               Log in
             </Typography>
           </Button>
         </div>
 
-        <div className = 'flexColumn flexSpaceAround'>
-          
+        <div className = 'flexColumn flexSpaceEvenly dimensions'>
+
+          <Typography variant="h5" className='color-purple'>
+            Register
+          </Typography>
+
           <TextField  id="standard-basic" label="First Name" variant="standard" />
           <TextField  id="standard-basic" label="Last Name" variant="standard" />
           <TextField  id="standard-basic" label="Nickname" variant="standard" />
           <TextField  id="standard-basic" label="Email" variant="standard" />
           <TextField  id="standard-basic" label="Password" variant="standard" />
 
-          <Button  variant="contained" >          
-            <Typography variant="h5" gutterBottom>
+          <Button  variant="contained" >
+            <Typography variant="h5" gutterBottom >
               Register
             </Typography>
           </Button>
