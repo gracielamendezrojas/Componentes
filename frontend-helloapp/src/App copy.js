b90d7f053e59c4016d985f8554023fdad9eab036
gracielamendezrojas <mmendezr@ucenfotec.ac.cr>
2023-06-08 18:20:35 -0600

    pagina realizada por Ignacio y Gra

---
+++ frontend-helloapp/src/App copy.js
@@ -0,0 +1,55 @@
+
+import './App.css';
+import Button from '@mui/material/Button';
+import homeImage from "./Images/chatimage.jpg";
+import hiImage from "./Images/HiImage.png";
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='flexEnd'>
+        <Link href="#" underline="hover" >
+          {<Typography variant="h6"  gutterBottom>
+            Home
+          </Typography>}
+        </Link>
+        <Link href="#" underline="hover" className='left-padding'>
+          {<Typography variant="h6" gutterBottom>
+            About us
+          </Typography>}
+        </Link>
+        <Link href="#" underline="hover" className='left-padding right-padding'>
+          {<Typography variant="h6" gutterBottom>
+            Login
+          </Typography>}        
+        </Link>
+      </header>
+      <div className='flexContainer flexSpaceAround'>
+        <div>
+          <Typography variant="h1" className='padding titleColor' gutterBottom>
+            HelloApp
+          </Typography>
+          <Typography variant="h6" className='padding color-gray' gutterBottom>
+            HelloApp offers you the opportunity to connect with friends, family, loved ones and others.
+          </Typography>
+          <Typography variant="h3" className='padding color-gray' gutterBottom>
+            Now is the time to chat online at no cost!
+          </Typography>
+          <img src={hiImage}  className='HiImage' alt="HelloApp Image"/>
+          <Button  variant="contained" >          
+            <Typography variant="h5" gutterBottom>
+              Let´s chat!
+            </Typography>
+          </Button>
+        </div>
+        <div className='flexEnd'>
+          <img src={homeImage}  className="homeImage" alt="Home Image"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- frontend-helloapp/src/App.js
+++ frontend-helloapp/src/App.js
@@ -1,51 +1,66 @@
 import './App.css';
 import Button from '@mui/material/Button';
-import homeImage from "./Images/chatimage.jpg";
-import hiImage from "./Images/HiImage.png";
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+
 
 function App() {
   return (
     <div className="App">
-      <header className='flexEnd'>
-        <Link href="#" underline="hover" >
-          {<Typography variant="h6"  gutterBottom>
-            Home
-          </Typography>}
-        </Link>
-        <Link href="#" underline="hover" className='left-padding'>
-          {<Typography variant="h6" gutterBottom>
-            About us
-          </Typography>}
-        </Link>
-        <Link href="#" underline="hover" className='left-padding right-padding'>
-          {<Typography variant="h6" gutterBottom>
-            Login
-          </Typography>}        
-        </Link>
-      </header>
-      <div className='flexContainer flexSpaceAround'>
-        <div>
-          <Typography variant="h1" className='padding titleColor' gutterBottom>
+      <header className='flexSpaceBetween'>
+        <div className='flexStart'>          
+          <Typography variant="h3" className='titleColor' gutterBottom>
             HelloApp
           </Typography>
-          <Typography variant="h6" className='padding color-gray' gutterBottom>
-            HelloApp offers you the opportunity to connect with friends, family, loved ones and others.
-          </Typography>
-          <Typography variant="h3" className='padding color-gray' gutterBottom>
-            Now is the time to chat online at no cost!
-          </Typography>
-          <img src={hiImage}  className='HiImage' alt="HelloApp Image"/>
+        </div>
+        <div className='flexEnd'>
+          <Link href="#" underline="hover" >
+            {<Typography variant="h6"  gutterBottom>
+              Home
+            </Typography>}
+          </Link>
+          <Link href="#" underline="hover" className='left-padding'>
+            {<Typography variant="h6" gutterBottom>
+              About us
+            </Typography>}
+          </Link>
+          <Link href="#" underline="hover" className='left-padding right-padding'>
+            {<Typography variant="h6" gutterBottom>
+              Login
+            </Typography>}        
+          </Link>
+        </div>
+        
+      </header>
+      <div className='flexContainer flexSpaceAround'>
+        <div className = 'flexColumn flexSpaceAround color-blue padding'>
+          
+          <TextField  id="standard-basic" label="Email" variant="standard" />
+          <TextField  id="standard-basic" label="Password" variant="standard" />
+
           <Button  variant="contained" >          
             <Typography variant="h5" gutterBottom>
-              Let´s chat!
+              Log in
             </Typography>
           </Button>
         </div>
-        <div className='flexEnd'>
-          <img src={homeImage}  className="homeImage" alt="Home Image"/>
+
+        <div className = 'flexColumn flexSpaceAround'>
+          
+          <TextField  id="standard-basic" label="First Name" variant="standard" />
+          <TextField  id="standard-basic" label="Last Name" variant="standard" />
+          <TextField  id="standard-basic" label="Nickname" variant="standard" />
+          <TextField  id="standard-basic" label="Email" variant="standard" />
+          <TextField  id="standard-basic" label="Password" variant="standard" />
+
+          <Button  variant="contained" >          
+            <Typography variant="h5" gutterBottom>
+              Register
+            </Typography>
+          </Button>
         </div>
+
       </div>
     </div>
   );
