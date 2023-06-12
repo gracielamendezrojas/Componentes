93aa44cb74bb3927deea41bff437470f684885ae
IGNACIO JOSUE ZELADA ARAYA <izeladaa@ucenfotec.ac.cr>
2023-06-12 15:12:19 -0600

    Hot Fix 1

--- frontend-helloapp/src/App.js
+++ frontend-helloapp/src/App.js
@@ -1,108 +1,52 @@
-import './App.css';
+
+import './LogIn.css';
 import Button from '@mui/material/Button';
+import homeImage from "./Images/chatimage.jpg";
+import hiImage from "./Images/HiImage.png";
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import TextField from '@mui/material/TextField';
-import {createMuiTheme, ThemeProvider} from '@mui/material/styles';
 
-
-const pTheme = createMuiTheme({
-  palette: {
-    primary:{
-      main: '#97A7CF'
-    },
-    text:{
-      primary: '#97A7CF'
-    }
-  }
-});
-
-const wTheme = createMuiTheme({
-  palette: {
-    primary:{
-      main: '#ffffff'
-    },
-    text:{
-      primary: '#ffffff'
-    }
-  }
-});
-
-
-function App() {
+function LogIn() {
   return (
     <div className="App">
-      <header className='flexSpaceBetween'>
-        <div className='flexStart'>          
-          <Typography variant="h3" className='titleColor' gutterBottom>
+      <header className='flexEnd'>
+        <Link href="#" underline="hover" >
+          {<Typography variant="h6"  gutterBottom>
+            Home
+          </Typography>}
+        </Link>
+        <Link href="#" underline="hover" className='left-padding'>
+          {<Typography variant="h6" gutterBottom>
+            About us
+          </Typography>}
+        </Link>
+        <Link href="#" underline="hover" className='left-padding right-padding'>
+          {<Typography variant="h6" gutterBottom>
+            Login
+          </Typography>}        
+        </Link>
+      </header>
+      <div className='flexContainer flexSpaceAround'>
+        <div>
+          <Typography variant="h1" className='padding titleColor' gutterBottom>
             HelloApp
           </Typography>
-        </div>
-        <div className='flexEnd width'>
-          <Link href="#" underline="hover" >
-            {<Typography variant="h6"  gutterBottom>
-              Home
-            </Typography>}
-          </Link>
-          <Link href="#" underline="hover" className='left-padding'>
-            {<Typography variant="h6" gutterBottom>
-              About us
-            </Typography>}
-          </Link>
-          <Link href="#" underline="hover" className='left-padding right-padding'>
-            {<Typography variant="h6" gutterBottom>
-              Login
-            </Typography>}        
-          </Link>
-        </div>
-        
-      </header>
-      <div className='flexContainer flexSpaceBetween'>
-        <div className = 'flexColumn flexSpaceEvenly b-color-purple dimensions '>
-
-          <Typography variant="h5" className='color-white'>
-            Log in
+          <Typography variant="h6" className='padding color-gray' gutterBottom>
+            HelloApp offers you the opportunity to connect with friends, family, loved ones and others.
           </Typography>
-
-          <ThemeProvider theme={wTheme}>
-            <TextField required label="Email"  variant="standard" focused />
-            <TextField required label="Password" variant="standard" focused />
-
-            <Button  variant="contained">
-              <Typography variant="h5" className='color-purple'>
-                Log in
-              </Typography>
-            </Button>
-
-          </ThemeProvider>
-
-
-        </div>
-
-        <div className = 'flexColumn flexSpaceEvenly dimensions'>
-
-          <Typography variant="h5" className='color-purple'>
-            Register
+          <Typography variant="h3" className='padding color-gray' gutterBottom>
+            Now is the time to chat online at no cost!
           </Typography>
-
-          <ThemeProvider theme={pTheme}>
-            <TextField required label="First Name" variant="standard" focused />
-            <TextField required label="Last Name" variant="standard" focused />
-            <TextField required label="Nickname" variant="standard" focused />
-            <TextField required label="Email" variant="standard" focused />
-            <TextField required label="Password" variant="standard" focused />
-
-            <Button  variant="contained" >
-              <Typography variant="h5" className='color-white' >
-                Register
-              </Typography>
-            </Button>
-
-          </ThemeProvider>
-
-
+          <img src={hiImage}  className='HiImage' alt="HelloApp Image"/>
+          <Button  variant="contained" >          
+            <Typography variant="h5" gutterBottom>
+              Let´s chat!
+            </Typography>
+          </Button>
         </div>
-
+        <div className='flexEnd'>
+          <img src={homeImage}  className="homeImage" alt="Home Image"/>
+        </div>
       </div>
     </div>
   );
