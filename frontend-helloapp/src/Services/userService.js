62ae30445d002ebe40a514538b471d846331fe7a
IgnacioZAraya <izeladaa@ucenfotec.ac.cr>
2023-08-15 17:49:52 -0600

    Listo para levantar servicio

--- frontend-helloapp/src/Services/userService.js
+++ frontend-helloapp/src/Services/userService.js
@@ -1,7 +1,7 @@
 
-export const ip = "http://18.117.79.249";
+export const ip = "http://13.58.174.57";
 
-//export const ip = "http://localhost:5005";
+// export const ip = "http://localhost:5005";
 
 
 export const getUserByEmailAndPassword = async (email, password)=> {
